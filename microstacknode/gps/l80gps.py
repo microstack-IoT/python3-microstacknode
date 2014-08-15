d537f07b4cb01f12bd40964aca34c7d44f16f7f9
Thomas Preston <thomasmarkpreston@gmail.com>
2014-08-15 14:36:39 +0100

    almost finished new gps module (with support for locus logging). Need to do final touches - see TODO

--- microstacknode/gps/l80gps.py
+++ microstacknode/gps/l80gps.py
@@ -4,102 +4,174 @@ import threading
 import subprocess
 import logging
 import time
+import serial
+import io
+import datetime
 logging.basicConfig(level=logging.DEBUG)
 
 
-PMTK_LOCUS_QUERY_STATUS = "$PMTK183*38\r\n"
-PMTK_LOCUS_ERASE_FLASH = "$PMTK184,1*22\r\n"
-PMTK_LOCUS_STOP_LOGGER  ="$PMTK185,1*23\r\n" # start/stop
-PMTK_Q_LOCUS_DATA = "$PMTK622,1*29\r\n"
+PMTK_LOCUS_QUERY_STATUS = '$PMTK183*38\r\n'
+PMTK_LOCUS_ERASE_FLASH = '$PMTK184,1*22\r\n'
+PMTK_LOCUS_STOP_LOGGER = '$PMTK185,1*23\r\n' # start/sto
+PMTK_Q_LOCUS_DATA_FULL = '$PMTK622,0*29\r\n'
+PMTK_Q_LOCUS_DATA_PARTIAL = '$PMTK622,1*29\r\n'
 
 
-class L80GPS(threading.Thread):
+class NMEAPacketNotFoundError(Exception):
+    pass
+
+
+class GPGLLInvalidError(Exception):
+    pass
+
+
+# class L80GPS(threading.Thread):
+class L80GPS(object):
     """Thread that reads a stream of L80 GPS protocol lines and stores the
     information."""
 
     def __init__(self, device="/dev/ttyAMA0"):
         super().__init__()
-        self.device = device
-        self.device_tx = open(self.device, "w")
-        self.device_rx = open(self.device, "r")
-        self._running = False
-        self._gpgll = None
-        self._pmtk_response = None
-        self._pmtk_rep_buf = None
-
-    def run(self):
-        self._running = True
-        for line in self.device_rx:
-            if ("GPGLL" in line
-                    and l80gps_checksum_is_valid(line)
-                    and gpgll_as_dict(line)[0]['data_valid'] == "A"):
-                        self._gpgll = line
-                        logging.debug("L80GPS:Setting GPGLL to {}"
-                                      .format(line))
-                        subprocess.call(['sudo su -c "echo 1 > '
-                                         '/sys/class/leds/led0/brightness"'],
-                                        shell=True)
-                        time.sleep(0.3)
-                        subprocess.call(['sudo su -c "echo 0 > '
-                                         '/sys/class/leds/led0/brightness"'],
-                                        shell=True)
-                        time.sleep(0.2)
-
-            elif "$PMTKLOG" in line:
-                logging.debug("L80GPS:Found PMTKLOG, adding to response:{}".format(
-                    line))
-                if self._pmtk_rep_buf is None:
-                    self._pmtk_rep_buf = line
-                else:
-                    self._pmtk_rep_buf += line
-
-            elif "$PMTK001" in line:
-                logging.debug("L80GPS:Found PMTKACK, dealing with response")
-                logging.debug("L80GPS:self._pmtk_rep_buf is " + self._pmtk_rep_buf)
-                buf = self._pmtk_rep_buf
-                self._pmtk_rep_buf = None
-                if self._pmtk_response is not None:
-                    self._pmtk_response(buf)
-
-            if self._running is False:
-                break
-
-    def stop(self):
-        self._running = False
-
-    def _pmtk_response(self, response):
-        """Acts on the PMTK response."""
-        logging.debug("L80GPS:Acting on response (Making callback)")
-        if self._pmtk_callback:
-            logging.debug("L80GPS:There is a callback")
-            self._pmtk_callback(response)
-
-    def locus_query(self, callback):
-        logging.debug("L80GPS:locus query")
-        self._pmtk_callback = callback
-        self.device_tx.write(PMTK_LOCUS_QUERY_STATUS)
-
-    def locus_erase(self):
-        logging.debug("L80GPS:locus erase")
-        self._pmtk_callback = None
-        self.device_tx.write(PMTK_LOCUS_ERASE_FLASH)
-
-    def locus_start_stop(self):
-        logging.debug("L80GPS:locus start_stop")
-        self._pmtk_callback = None
-        self.device_tx.write(PMTK_LOCUS_STOP_LOGGER)
-
-    def locus_query_data(self, callback):
-        logging.debug("L80GPS:locus query_data")
-        self._pmtk_callback = callback
-        self.device_tx.write(PMTK_LOCUS_QUERY_STATUS)
+        self.device_tx_rx = serial.Serial(device,
+                                          baudrate=9600,
+                                          bytesize=8,
+                                          parity='N',
+                                          stopbits=1,
+                                          timeout=0.5,
+                                          rtscts=0)
 
     @property
     def gpgll(self):
-        if self._gpgll is None:
-            return "$GPGLL,3110.2908,N,12123.2348,E,041139.000,A,A*59"
+        pkt = self.get_nmea_pkt('GPGLL')
+        gpgll_dict, checksum = gpgll_as_dict(pkt)
+        if gpgll_dict['data_valid'] == "A":
+            return gpgll_dict
         else:
-            return self._gpgll
+            raise GPGLLInvalidError("Indicated by data_valid field.")
+
+    def locus_query(self):
+        """Returns the status of the locus logger."""
+        self.send_nmea_pkt(PMTK_LOCUS_QUERY_STATUS)
+        pmtklog_dict, checksum = pmtklog_as_dict(self.get_nmea_pkt('PMTKLOG'))
+        return pmtklog_dict
+
+    def locus_erase(self):
+        """Erases the internal log."""
+        self.send_nmea_pkt(PMTK_LOCUS_ERASE_FLASH)
+
+    def locus_start_stop(self):
+        """Starts or stops the logger."""
+        self.send_nmea_pkt(PMTK_LOCUS_STOP_LOGGER)
+
+    def locus_query_data(self):
+        """Returns a byte array of the log data (you can parse this later).
+
+        Example packets returned:
+
+        Data: $PMTKLOX,1,0,0100010B,1F000000,0F000000,0000100B,00000000,
+              00000000,00000003,FFFFFFFF,FFFFFFFF,FFFFFFFF,FFFFFFFF,
+              FFFFFFFF,FFFFFFFF,FFFFFFFF,FFFFFFFF,00FC8C1C,0DE9E753,
+              02A54356,42777508,C0A300C9,1CE9E753,02A14356,42397508,
+              C0A30092*2E
+
+        """
+        locus_data_ptn_start = 'PMTKLOX,0'
+        locus_data_ptn = 'PMTKLOX,1'
+        locus_data_ptn_end = 'PMTKLOX,2'
+        self.send_nmea_pkt(PMTK_Q_LOCUS_DATA_PARTIAL)
+
+        # get the start packet (not that we do anything with it, the returned
+        # number of packets doesn't even equal what this packet says it will!)
+        pkt = self.get_nmea_pkt(locus_data_ptn_start)
+        pkt, checksum = pkt.split('*') # data is already confirmed valid
+        message_id, type, num_pkts = pkt.split(',')
+
+        # get all the data packets until the end packet pattern if found
+        databytes = bytearray()
+        for i in range(int(num_pkts)):
+            pkt = self.get_nmea_pkt(locus_data_ptn)
+            pkt, checksum = pkt.split('*') # data is already confirmed valid
+            pkt_list = pkt.split(',')
+            # do some sanity checking
+            message_id, lox_type, index = pkt_list[:3]
+            # print("Hi", message_id, lox_type, index)
+            assert message_id == '$PMTKLOX'
+            assert lox_type == '1'
+            assert int(index) == i
+            # put the data into bytes
+            for hexstr in pkt_list[3:]:
+                databytes += hexstr2bytearray(hexstr)
+        # end pkt
+        pkt = self.get_nmea_pkt(locus_data_ptn_end)
+        return databytes
+
+    def parse_locus_data(self, data, format='basic'):
+        """Returns the data in a sensible structure according to the format."""
+        # assuming basic format
+        # utc (4)
+        # fix (1)
+        # lat (4)
+        # lon (4)
+        # height (2)
+        # checksum (1)
+        data.reverse()
+        sensible_data = []
+        while True:
+            try:
+                utc = [data.pop(), data.pop(), data.pop(), data.pop()]
+                # print("utc", utc)
+                fix = data.pop()
+                # print("fix", fix)
+                lat = [data.pop(), data.pop(), data.pop(), data.pop()]
+                # print("lat", lat)
+                lon = [data.pop(), data.pop(), data.pop(), data.pop()]
+                # print("lon", lon)
+                height = [data.pop(), data.pop()]
+                # print("height", height)
+                checksum = data.pop()
+                # print("checksum", checksum)
+            except IndexError:
+                return sensible_data
+            else:
+                utc = parse_long(utc)
+                if utc >= 0xffffffff:
+                    continue
+                # TODO also parse checksum
+                # TODO fix lat/long (they report None)
+                sensible_data.append({'utc': datetime.date.fromtimestamp(utc),
+                                      'fix': fix,
+                                      'lat': parse_float(lat),
+                                      'lon': parse_float(lon),
+                                      'height': parse_int(height),
+                                      'checksum': checksum})
+
+
+    def get_nmea_pkt(self, pattern):
+        """Returns the next valid NMEA which contains the pattern provided.
+        For example:
+
+            gps.get_nmea_pkt('GPRMC')
+
+        Returns
+
+            $GPRMC,013732.000,A,3150.7238,N,11711.7278,E,0.00,0.00,220413,,,A*68
+
+        """
+        pattern_bytes = bytes(pattern, 'utf-8')
+        while True:
+            line = self.device_tx_rx.readline()
+            # logging.debug("L80GPS:readline returned - "+str(line))
+            if line == b'':
+                raise NMEAPacketNotFoundError(
+                    "Timed out before valid '{}'.".format(pattern))
+            elif not l80gps_checksum_is_valid(line):
+                continue
+            elif pattern_bytes in line:
+                return str(line, 'utf-8')
+
+    def send_nmea_pkt(self, pkt):
+        """Write pkt to the serial port."""
+        self.device_tx_rx.write(bytes(pkt, 'utf-8'))
 
 
 def gpgll_as_dict(gpgll_str):
@@ -120,21 +192,120 @@ def gpgll_as_dict(gpgll_str):
     gpgll, checksum = gpgll_str[1:].split("*")  # remove `$` split *
     message_id, latitude, ns, longitude, ew, utc, data_valid, pos_mode = \
         gpgll.split(",")
-    return ({"message_id": message_id,
-             "latitude": float(latitude),
-             "ns": ns,
-             "longitude": float(longitude),
-             "ew": ew,
-             "utc": float(utc),
-             "data_valid": data_valid,
-             "pos_mode": pos_mode},
-            checksum)
+    latitude = 0.0 if latitude == '' else latitude
+    longitude = 0.0 if longitude == '' else longitude
+    utc = 0.0 if utc == '' else utc
+    gpgll_dict = ({"message_id": message_id,
+                   "latitude": float(latitude),
+                   "ns": ns,
+                   "longitude": float(longitude),
+                   "ew": ew,
+                   "utc": float(utc),
+                   "data_valid": data_valid,
+                   "pos_mode": pos_mode},
+                  checksum)
+    # logging.debug("L80GPS:Converting '{}'\ninto: {}".format(gpgll_str, gpgll_dict))
+    return gpgll_dict
+
+
+def pmtklog_as_dict(pmtklog_str):
+    """Returns the PMTKLOG as a dictionary and the checksum.
+
+         in: $PMTKLOG,456,0,11,31,2,0,0,0,3769,46*48
+        out: ({'message_id': 'PMTKLOG',
+               'serial': 456,
+               'type': 0,
+               'mode': 11,
+               'content': 31,
+               'interval': 2,
+               'distance': 0,
+               'speed': 0,
+               'status': 0,
+               'number': 3769,
+               'percent': 46},
+              48)
+    """
+    pmtklog, checksum = pmtklog_str[1:].split('*')  # remove `$` split *
+    message_id, serial, type, mode, content, interval, distance, speed,\
+        status, number, percent = pmtklog.split(',')
+    pmtklog_dict = ({'message_id': 'PMTKLOG',
+                     'serial': serial,
+                     'type': type,
+                     'mode': mode,
+                     'content': content,
+                     'interval': interval,
+                     'distance': distance,
+                     'speed': speed,
+                     'status': status,
+                     'number': number,
+                     'percent': percent},
+                    checksum)
+    return pmtklog_dict
 
 
 def l80gps_checksum_is_valid(gps_str):
-    """Returns True if the checksum is valid in an GPS L80 protocol packet."""
-    gpgll, checksum = gps_str[1:].split("*")  # remove `$` split *
-    check = 0
-    for char in gpgll:
-        check ^= ord(char)
-    return check == int(checksum, 16)  # checksum is hex string
+    """Returns True if the checksum is valid in an GPS L80 protocol line.
+
+        !!!! This method assumes gps_str is a byte string. !!!!
+
+    """
+    if gps_str[0] != ord(b'$'):
+        return False
+    try:
+        gpgll, checksum = gps_str[1:].split(b'*')  # remove `$` split *
+    except:
+        # logging.debug("L80GPS:Invalid GPS str")
+        # logging.debug(gps_str)
+        return False
+    else:
+        check = 0
+        for char in gpgll:
+            # logging.debug("char is"+char)
+            # check ^= ord(char)
+            check ^= char
+        return check == int(checksum, 16)  # checksum is hex string
+
+
+def hexstr2bytearray(s):
+    """Converts a string of hex characters to bytes.
+
+        'DEADBEEF'
+
+    becomes
+
+        bytearray(b'\xde\xad\xbe\xef')
+
+    """
+    # split the string into a list of strings, each two characters long
+    # then turn each two character string into an int (base 16) and put
+    # that array into a bytearray
+    n = 2
+    return bytearray([int(s[i:i+n], 16) for i in range(0, len(s), n)])
+
+
+def parse_float(bytes):
+    longValue = parse_long(bytes)
+
+    # borrowed code from https://github.com/douggilliland/Dougs-Arduino-Stuff/blob/master/Host%20code/parseLOCUS/parseLOCUS.cpp
+    exponent = ((longValue >> 23) & 0xff) # float
+    exponent -= 127.0
+    exponent = pow(2,exponent)
+    mantissa = (longValue & 0x7fffff)
+    mantissa = 1.0 + (mantissa/8388607.0)
+    floatValue = mantissa * exponent
+    if ((longValue & 0x80000000) == 0x80000000):
+        floatValue = -floatValue
+
+
+def parse_long(bytes):
+    if len(bytes) != 4:
+        print >> sys.stderr, "WARNING: expecting 4 bytes got %s" % bytes
+    number = ((0xFF & bytes[3]) << 24) | ((0xFF & bytes[2]) << 16) | ((0xFF & bytes[1]) << 8) | (0xFF & bytes[0])
+    return number
+
+
+def parse_int(bytes):
+    if len(bytes) != 2:
+        print >> sys.stderr, "WARNING: expecting 2 bytes got %s" % bytes
+    number = ((0xFF & bytes[1]) << 8) | (0xFF & bytes[0])
+    return number
--- tests/test_gps.py
+++ tests/test_gps.py
@@ -11,10 +11,43 @@ import microstacknode.gps.l80gps
 class TestL80GPS(unittest.TestCase):
     def setUp(self):
         self.gps = microstacknode.gps.l80gps.L80GPS()
-        self.gps.start()
 
+    @unittest.skip
     def test_locus_query(self):
-        self.gps.locus_query(lambda r: print(r))
+        self.gps.locus_query()
+
+    @unittest.skip
+    def test_locus_erase(self):
+        pass
+        # self.gps.locus_erase()
+
+    @unittest.skip
+    def test_locus_start_stop(self):
+        pass
+        # self.gps.locus_start_stop()
+
+    @unittest.skip
+    def test_locus_query_data(self):
+        self.gps.locus_query_data()
+
+    def test_locus_query_data_and_parsing(self):
+        print("Getting data.")
+        attempt = 0
+        success = False
+        while success == False and attempt < 5:
+            try:
+                data = self.gps.locus_query_data()
+            except microstacknode.gps.l80gps.NMEAPacketNotFoundError:
+                print("Getting data failed, trying again.")
+                attempt += 1
+            else:
+                success = True
+        self.assertTrue(success)
+        print(data)
+        print("=================\nParsing data.\n=================")
+        for d in self.gps.parse_locus_data(data):
+            print(d)
+
 
 if __name__ == "__main__":
     unittest.main()
