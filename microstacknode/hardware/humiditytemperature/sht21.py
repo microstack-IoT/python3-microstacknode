a2175e0c0ec0f38afcac6483f56d411cfad81079
Thomas Preston <thomasmarkpreston@gmail.com>
2016-01-12 17:14:45 +0000

    converted sht21 to use visitor pattern

--- microstacknode/hardware/humiditytemperature/sht21.py
+++ microstacknode/hardware/humiditytemperature/sht21.py
@@ -3,45 +3,54 @@ from microstackcommon.i2c import I2CMaster, writing_bytes, writing, reading
 
 
 DEFAULT_I2C_BUS = 1
+DEFAULT_I2C_ADDRESS = 0x40
+
+# control constants
+CMD_SOFTRESET = 0xFE
+CMD_TEMPERATURE_NO_HOLD = 0xF3
+CMD_HUMIDITY_NO_HOLD = 0xF5
 
 
 class ChecksumFailedError(Exception):
     pass
 
 
-class SHT21(I2CMaster):
+class SHT21():
     """SHT21 temperature and humidity sensor."""
 
-    _I2C_ADDRESS = 0x40
-
-    # control constants
-    _CMD_SOFTRESET = 0xFE
-    _CMD_TEMPERATURE_NO_HOLD = 0xF3
-    _CMD_HUMIDITY_NO_HOLD = 0xF5
-
-    def __init__(self, i2c_bus=DEFAULT_I2C_BUS):
-        super().__init__(i2c_bus)
+    def __init__(self,
+                 i2c_master=I2CMaster(DEFAULT_I2C_BUS),
+                 i2c_reading=reading,
+                 i2c_writing_bytes=writing_bytes,
+                 i2c_addr=DEFAULT_I2C_ADDRESS):
+        self.i2c_master = i2c_master
+        self.i2c_reading = i2c_reading
+        self.i2c_writing_bytes = i2c_writing_bytes
+        self.i2c_addr = i2c_addr
 
     def __enter__(self):
-        self = super().__enter__()
+        self.i2c_master = self.i2c_master.__enter__()
         self.reset()
         return self
 
+    def __exit__(self, *args):
+        self.i2c_master.__exit__(*args)
+
     def reset(self):
         """Resets the SHT12."""
-        self.transaction(
-            writing_bytes(self._I2C_ADDRESS, self._CMD_SOFTRESET))
+        self.i2c_master.transaction(
+            self.i2c_writing_bytes(self.i2c_addr, CMD_SOFTRESET))
         time.sleep(0.050)
 
     def get_temperature(self):
         """Reads the temperature from the sensor. This call blocks
         for 250ms to allow the sensor to return the data.
         """
-        self.transaction(
-            writing_bytes(self._I2C_ADDRESS,
-                          self._CMD_TEMPERATURE_NO_HOLD))
+        self.i2c_master.transaction(
+            self.i2c_writing_bytes(self.i2c_addr, CMD_TEMPERATURE_NO_HOLD))
         time.sleep(0.250)
-        data = self.transaction(reading(self._I2C_ADDRESS, 3))[0]
+        data = self.i2c_master.transaction(
+            self.i2c_reading(self.i2c_addr, 3))[0]
         if _calculate_checksum(data, 2) != data[2]:
             raise ChecksumFailedError("Temperature checksum failed.")
         else:
@@ -50,11 +59,11 @@ class SHT21(I2CMaster):
     def get_humidity(self):
         """Reads the humidity from the sensor. This call blocks
         for 250ms to allow the sensor to return the data"""
-        self.transaction(
-            writing_bytes(self._I2C_ADDRESS,
-                          self._CMD_HUMIDITY_NO_HOLD))
+        self.i2c_master.transaction(
+            self.i2c_writing_bytes(self.i2c_addr, CMD_HUMIDITY_NO_HOLD))
         time.sleep(0.250)
-        data = self.transaction(reading(self._I2C_ADDRESS, 3))[0]
+        data = self.i2c_master.transaction(
+            self.i2c_reading(self.i2c_addr, 3))[0]
         if _calculate_checksum(data, 2) != data[2]:
             raise ChecksumFailedError("Humidity checksum failed.")
         else:
